293e2119ed854debfae6af6864f0395391ae6202
unknown <domanhhai209@gmail.com>
2022-10-07 15:10:25 +0700

    fix js

--- assets/js/main.js
+++ assets/js/main.js
@@ -65,51 +65,10 @@ $(function ($) {
       $(this).parent().next().slideToggle();
     })
 
-setTimeout(function() {
+  setTimeout(function() {
        $('.split').addClass('active')
     }, 400);
-  var Split = function() {
-  this.$t = $(".split");    
-  this.gridX = 6;
-  this.gridY = 4;
-  this.w = this.$t.width();
-  this.h = this.$t.height();
-  this.img = $("img", this.$t).attr("src");
-  // this.delay = 0.3;
- 
-  this.create = function() {
-    $("div", this.$t).remove();
-    
-    var x = 0;
-    var y = 0;
-    for (x = 0; x < this.gridX; x++) {
-      for (y = 0; y < this.gridY; y++) {
-        // var width = this.w / this.gridX * 101 / this.w + "%",
-        //     height = this.h / this.gridY * 101 / this.h + "%",
-          var  top = this.h / this.gridY * y * 100 / this.h + "%",
-            left = this.w / this.gridX * x * 100 / this.w + "%",
-            bgPosX = -(this.w / this.gridX * x) + "px",
-            bgPosY = -(this.h / this.gridY * y) + "px";
- 
-        $("<div />")
-          .css({
-          top: top,
-          left: left,
-          // width: width,
-          // height: height,
-          backgroundImage: "url(" + this.img + ")",
-          backgroundPosition: bgPosX + " " + bgPosY,
-          backgroundSize: this.w + "px"
-          // transitionDelay: x * this.delay + y * this.delay + "s"
-        })
-          .appendTo(this.$t);
-      }
-    }
-  };
- 
-  this.create();
-
-};
+  
  
 window.onload = function() {
   var split = new Split();
