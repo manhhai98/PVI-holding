03d1a70295822cd5802d81d8785e3eb358c56824
unknown <domanhhai209@gmail.com>
2023-03-04 08:40:20 +0700

    update 3/4/2023

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,49 +3,51 @@ $(function ($) {
 
   jQuery(document).ready(function () {
     $(document).click(function (e) {
-        var $searchForm = $(".search");
-       
-        if($searchForm !== e.target && !$searchForm.has(e.target).length){
-            $(".search-input").removeClass('open');
-        }  
+      var $searchForm = $(".search");
+
+      if ($searchForm !== e.target && !$searchForm.has(e.target).length) {
+        $(".search-input").removeClass('open');
+      }
     })
 
-    $('.search .btn').on('click', function(){
+
+
+    $('.search .btn').on('click', function () {
       $('.search-input').toggleClass('open');
     })
 
-    $(window).on('scroll', function(){
+    $(window).on('scroll', function () {
       var scroll = $(window).scrollTop();
 
-      if(scroll >= 130){
+      if (scroll >= 130) {
         $('.header-bottom').addClass('animate__animated animate__fadeInDown fixed');
         $('.back-to-top').addClass('animate__animated animate__fadeInRight show');
-      }else{
+      } else {
         $('.header-bottom').removeClass('animate__animated animate__fadeInDown fixed');
         $('.back-to-top').removeClass('animate__animated animate__fadeInRight show');
       }
     })
-    
-    $('.menu-btn').on('click', function(){
+
+    $('.menu-btn').on('click', function () {
       $('.mb-menu').addClass('open');
       $('body').addClass('hidden-overflow');
     })
 
-    $('.mb-menu .close').on('click', function(){
+    $('.mb-menu .close').on('click', function () {
       $('.mb-menu').removeClass('open');
       $('body').removeClass('hidden-overflow');
     })
 
-    $('.mb-menu .dropdown-link').on('click', function(){
-        $(this).parent().next().slideToggle();
-        $(this).children('.fa-minus-square').toggle();
-        $(this).children('.fa-plus-square').toggle();
-        $('.mb-menu .dropdown-link').not(this).parent().next().slideUp();
-        $('.mb-menu .dropdown-link').not(this).children('.fa-minus-square').hide();
-        $('.mb-menu .dropdown-link').not(this).children('.fa-plus-square').show();
-      })
+    $('.mb-menu .dropdown-link').on('click', function () {
+      $(this).parent().next().slideToggle();
+      $(this).children('.fa-minus-square').toggle();
+      $(this).children('.fa-plus-square').toggle();
+      $('.mb-menu .dropdown-link').not(this).parent().next().slideUp();
+      $('.mb-menu .dropdown-link').not(this).children('.fa-minus-square').hide();
+      $('.mb-menu .dropdown-link').not(this).children('.fa-plus-square').show();
+    })
 
-    $('.detail-content .item .text').on('click', function(){
+    $('.detail-content .item .text').on('click', function () {
       $(this).next().slideToggle();
       $('.detail-content .item .text').not(this).next().slideUp();
       $(this).children('.fa-caret-square-right').toggle();
@@ -56,15 +58,15 @@ $(function ($) {
 
     $('.commit .row div:nth-child(2n)').after('<hr>');
 
-    $('.contact-form input,.contact-form textarea').on('click', function(){
+    $('.contact-form input,.contact-form textarea').on('click', function () {
       $(this).addClass('active');
     })
 
-    $('.contact-form input,.contact-form textarea').on('focus', function(){
+    $('.contact-form input,.contact-form textarea').on('focus', function () {
       $(this).addClass('active');
     })
 
-    $('.coucil a.name').on('click', function(){
+    $('.coucil a.name').on('click', function () {
       $(this).parent().next().slideToggle();
     })
 
@@ -74,81 +76,82 @@ $(function ($) {
     };
     setHeight();
 
-    $(window).resize(function() {
+    $(window).resize(function () {
       setHeight();
     });
-  
+
+    $('#banner-slide').css('height', $(window).innerHeight() - $('.header').innerHeight() - $('.pvi-news').innerHeight())
 
     $('#banner-slide').owlCarousel({
-      loop:false,
-      margin:0,
-      nav:true,
-      navText:["<i class='fas fa-arrow-left'></i>","<i class='fas fa-arrow-right'></i>"],
-      dots:false,
-      responsive:{
-          0:{
-              items:1
-          },
-          600:{
-              items:1
-          },
-          1000:{
-              items:1
-          }
+      loop: false,
+      margin: 0,
+      nav: true,
+      navText: ["<i class='fas fa-arrow-left'></i>", "<i class='fas fa-arrow-right'></i>"],
+      dots: false,
+      responsive: {
+        0: {
+          items: 1
+        },
+        600: {
+          items: 1
+        },
+        1000: {
+          items: 1
+        }
       }
     })
 
     $('#members-slide').owlCarousel({
-      loop:false,
-      margin:22,
-      nav:false,
-      dots:false,
-      responsive:{
-          0:{
-              items:1
-          },
-          767:{
-              items:2
-          },
-          1200:{
-              items:3
-          }
+      loop: false,
+      margin: 22,
+      nav: false,
+      dots: false,
+      responsive: {
+        0: {
+          items: 1
+        },
+        767: {
+          items: 2
+        },
+        1200: {
+          items: 3
+        }
       }
     });
 
     $('.partners-slide').owlCarousel({
-      loop:false,
-      margin:22,
-      nav:false,
-      dots:false,
-      responsive:{
-          0:{
-              items:2
-          },
-          767:{
-              items:4
-          },
-          1200:{
-              items:6
-          }
+      loop: false,
+      margin: 22,
+      nav: false,
+      dots: false,
+      responsive: {
+        0: {
+          items: 2
+        },
+        767: {
+          items: 4
+        },
+        1200: {
+          items: 6
+        }
       }
     })
 
     $('.awards-slide').owlCarousel({
-      loop:false,
-      margin:22,
-      nav:false,
-      dots:false,
-      responsive:{
-          0:{
-              items:1
-          },
-          767:{
-              items:2
-          },
-          1200:{
-              items:3
-          }
+      loop: false,
+      margin: 22,
+      nav: false,
+      dots: false,
+      responsive: {
+        0: {
+          items: 1
+        },
+        767: {
+          items: 2
+        },
+        1200: {
+          items: 3
+        }
       }
     })
   });
